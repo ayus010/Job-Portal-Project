cbf4f68f8f3e56ef00a64d20e4b9c62981cf6f27
ayus010 <ayushkumar85272@gmail.com>
2025-04-10 11:17:32 +0530

    Footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <div class="footer-copyright-item">
   <div class="container">
-    <p>This website is coded by Md. Sabbir Hosen &copy; <script>
+    <p>This website is coded by Ayush Kumar &copy; <script>
         const date = new Date();
         const year = date.getFullYear();
         document.write(year);
